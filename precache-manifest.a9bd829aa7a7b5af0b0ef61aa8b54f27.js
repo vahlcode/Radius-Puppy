e8e3b08f3134f22d1c15cda3c9d0cbff4fd55b67
Valentine Elum <valentineelum@gmail.com>
2020-08-18 15:46:51 +0100

    Updates

---
+++ precache-manifest.a9bd829aa7a7b5af0b0ef61aa8b54f27.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f6fde188a075d711f3fff811a4cce3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "649afec795b6befbdc0b",
+    "url": "./static/css/2.2b084824.chunk.css"
+  },
+  {
+    "revision": "a777533fa2c0c2eeb410",
+    "url": "./static/css/main.8b5afbcc.chunk.css"
+  },
+  {
+    "revision": "649afec795b6befbdc0b",
+    "url": "./static/js/2.007be2e2.chunk.js"
+  },
+  {
+    "revision": "78cb66eee2280b346dbdfdcf839e78ee",
+    "url": "./static/js/2.007be2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a777533fa2c0c2eeb410",
+    "url": "./static/js/main.c7ebadf2.chunk.js"
+  },
+  {
+    "revision": "128c2af4f47cb0c7f575",
+    "url": "./static/js/runtime-main.e04afe05.js"
+  },
+  {
+    "revision": "23730afabdb4fe6ec27682872cf00084",
+    "url": "./static/media/bg-pattern.23730afa.svg"
+  },
+  {
+    "revision": "3139583797c32cf12391dd4f8bb4ce91",
+    "url": "./static/media/bg.31395837.svg"
+  },
+  {
+    "revision": "7083b8c447fec0ed84d30356bd7a9e50",
+    "url": "./static/media/icomoon.7083b8c4.svg"
+  },
+  {
+    "revision": "8fceb3abcf91afdd5ec12994d7f4165c",
+    "url": "./static/media/icomoon.8fceb3ab.eot"
+  },
+  {
+    "revision": "c74d019a3f19b1559d77c5120c7d9903",
+    "url": "./static/media/icomoon.c74d019a.ttf"
+  },
+  {
+    "revision": "f0ee518602a18f139c8464d257c18063",
+    "url": "./static/media/icomoon.f0ee5186.woff"
+  },
+  {
+    "revision": "a0a914c40945ceab7382d42bff13d061",
+    "url": "./static/media/logo.a0a914c4.svg"
+  }
+]);
